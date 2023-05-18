66fc1148875af1b6ec2b36f1a01df694a67203e0
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-05-17 21:24:45 -0400

    add wildcard endpoint

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 // Imports
 const express = require('express')
+const path = require('path')
 const routes = require('./routes')
 
 // Port
@@ -14,7 +15,12 @@ app.use(express.static('public'))
 // Routes
 app.use(routes)
 
+// Wildcard will return index.html
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 // Port 3001 Setup
 app.listen(PORT, () => {
-    console.log(`Application is running on port: ${PORT} 🚀`) 
+	console.log(`Application is running on port: ${PORT} 🚀`)
 })
