e07272d57be4436a28922a1dbd3c7c8008888be1
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-05-22 16:23:30 -0400

    Pushing to Heroku

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const routes = require('./routes')
 
 // Port
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT|| 3001
 
 // Express Application
 const app = express()
